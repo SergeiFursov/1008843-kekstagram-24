f0ba350c6ec351d6015e9eb871b74448d34cdbed
Sergei Fursov <fursov_sergei@mail.ru>
2021-10-16 15:13:36 +0500

    СÐоздал модули

---
+++ js/util.js
@@ -0,0 +1,30 @@
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex ;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min >= max) {
+    return;
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function createIdGenerator () {
+  let lastGeneratedId = 0;
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+export {createIdGenerator, shuffle, getRandomNumber};
