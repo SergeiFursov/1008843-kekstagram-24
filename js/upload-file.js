da3e66f8763a734cb6bf868aadcdb68dd562aa08
Сергей Фурсов <50398195+SergeiFursov@users.noreply.github.com>
2021-11-02 19:28:21 +0500

    Update upload-file.js

--- js/upload-file.js
+++ js/upload-file.js
@@ -4,7 +4,7 @@ const userFormElement = document.querySelector('.img-upload__overlay');
 const userFormOpenElement = document.querySelector('#upload-file');
 const userFormCloseElement = document.querySelector('#upload-cancel');
 const bodyElement = document.querySelector('body');
-
+const closeFormEsc = document.querySelector('.img-upload__form');
 
 const onFormEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
