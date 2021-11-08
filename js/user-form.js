ea47b3ddd950612ef118e7b8b0c86e5c0a0ce288
Sergei Fursov <fursov_sergei@mail.ru>
2021-11-08 17:56:36 +0500

    8.11. Правда или действие (часть 1)

--- js/user-form.js
+++ js/user-form.js
@@ -16,21 +16,19 @@ commentUserInput.addEventListener('input', () => {
 
 // Валидация хэштега
 
-// Валидация хэштега
 const validateHashtag = (hashtag) => {
-  const regexp = /^#[A-Za-zА-Яа-яЁё0-9]+$/;
-
-  const startsWithHash = !hashtag.startsWith('#');
+  const regexp = /[A-Za-zА-Яа-яЁё0-9]+$/;
+  const startString = !hashtag.startsWith('#');
   const isLong = hashtag.length > MAX_HASHTAG_LENGTH;
   const hasRestrictedSymbols = !regexp.test(hashtag);
 
   switch (true) {
-    case startsWithHash:
+    case startString:
       return 'Хэштег должен начинаться с символа # ';
     case isLong:
       return 'Слишком длинный хэштег';
     case hasRestrictedSymbols:
-      return 'Хэштег должен начинаться с символа #, минимум 2 симв.';
+      return 'Только буквы и цифры';
     default:
       return '';
   }
@@ -38,42 +36,42 @@ const validateHashtag = (hashtag) => {
 
 textHashtagInput.addEventListener('input', () => {
   let message = '';
+
   const textHashtagValue = textHashtagInput.value;
   const strHashtags = textHashtagValue.toLowerCase().split(' ');
 
   // Проверка максимальной длины массива хэштегов. Если хэштегов больше 5, то возвращаем ошибку и прерываем выполнение
   if (strHashtags.length > MAX_HASHTAG_COUNT) {
     textHashtagInput.setCustomValidity('Должно быть не больше 5 хэштегов');
-    textHashtagInput.reportValidity();
-    return;
+    //return;
   }
 
   // Если длина массива правильная, то проверяем каждый отдельный хэштег
-  for (let index = 0; index < strHashtags; index++) {
+  for (let index = 0; index < strHashtags.length; index++) {
+
     const hashtag = strHashtags[index];
-
-    // Проверяем, встречается ли данных хэштег еще раз в этом массиве
-    const hasDuplicates = strHashtags.indexOf(hashtag, index);
-
-    // Если встречается, то возвращаем ошибку и прерываем выполнение
-    if (hasDuplicates !== -1) {
-      message = 'Хэштег не должен повторяться';
-      break;
-    }
 
     // Если дубликатов нет, то проверяем хэштег по остальным критериям
     const error = validateHashtag(hashtag);
-
     // Если хэштег провалил проверку, то возвращаем ошибку и прерываем выполнение массива
     if (error) {
       message = error;
       break;
     }
-    textHashtagInput.setCustomValidity(message);
+    // Проверяем, встречается ли данных хэштег еще раз в этом массиве
+
+    const hasDuplicates = strHashtags.indexOf(hashtag, index + 1);
+
+    // Если встречается, то возвращаем ошибку и прерываем выполнение
+
+    if (hasDuplicates !== -1) {
+      message = 'Хэштег не должен повторяться';
+      break;
+    }
+    // Здесь выводим сообщение об ошибке или обнуляем его, если ошибок не было
+
   }
-
-  // Здесь выводим сообщение об ошибке или обнуляем его, если ошибок не было
-
+  textHashtagInput.setCustomValidity(message);
   textHashtagInput.reportValidity();
 });
 
