03b11827df9841c6ac1c27586e11a8044122ec74
Sergei Fursov <fursov_sergei@mail.ru>
2021-11-11 00:55:05 +0500

    Get comment

--- js/photo-view.js
+++ js/photo-view.js
@@ -1,7 +1,7 @@
 import {miniatures} from './miniatures.js';
 import {bodyElement} from './upload-file.js';
 import {isEscapeKey} from './util.js';
-import { INITIAL_NUMBER_COMMENTS } from './data.js';
+
 
 const bigPictureOpen = document.querySelector('.big-picture');
 const pictureThumbnails = document.querySelectorAll('.picture');
@@ -11,46 +11,31 @@ const likesCount = document.querySelector('.likes-count');
 const totalComments= document.querySelector('.comments-count');
 const closeUserBigPicture = document.querySelector('.big-picture__cancel');
 const descriptionBigPicture = document.querySelector('.social__caption');
-const socialComments = document.querySelector('.social__comments');
+const commentsList = document.querySelector('.social__comments');
 
-const commentsCount = document.querySelector('.social__comment-count');
+//const commentsCount = document.querySelector('.social__comment-count');
 const socialComment = document.querySelector('.social__comment');
 const commentsFragment = document.createDocumentFragment();
 
 //Добавляем комментарии
 
-for (let index = 0; index < miniatures.length; index++) {
+function getComments() {
   const commentElement = socialComment.cloneNode(true);
-  commentsFragment.append(commentElement);
-  if (index > 2) {
-    commentElement.classList.add('hidden');
-  }
+  commentsFragment.appendChild(commentElement);
+  commentsList.appendChild(commentsFragment);
 }
 
-socialComments.append(commentsFragment);
+for (let index = 0; index < 3; index++) {
+  getComments(miniatures[index]);
+}
 
-// Счетчик комментариев
-
-const commentsUsers = document.querySelectorAll('.social__comment');
 const buttonUploadComments = document.querySelector('.comments-loader');
 
-let counter;
-const onCommentLoad = () => {
-  const total = totalComments.textContent;
-  counter = total;
-  commentsCount.textContent = ` ${counter} из ${total} комментариев`;
-  if (counter === total) {
-    buttonUploadComments.classList.add('hidden');
+buttonUploadComments.addEventListener('click', () => {
+  for (let index = 0; index < 3; index++) {
+    getComments(miniatures[index]);
   }
-
-  for (let index = 0; index < commentsUsers.length; index++) {
-    if (commentsUsers[index].classList.contains('hidden')) {
-      commentsUsers[index].classList.remove('hidden');
-    }
-  }
-};
-
-buttonUploadComments.addEventListener('click', onCommentLoad);
+});
 
 // Закрываем по ESC
 
@@ -60,12 +45,6 @@ const onFullScreenEscKeydown = (evt) => {
     bigPictureOpen.classList.add('hidden');
     bodyElement.classList.remove('modal-open');
     buttonUploadComments.classList.remove('hidden');
-    counter = INITIAL_NUMBER_COMMENTS;
-    commentsCount.textContent = ` ${counter} из ${totalComments.textContent} комментариев`;
-
-    for (let index = 0; index < commentsUsers.length - 5; index++) {
-      commentsUsers[index].classList.add('hidden');
-    }
   }
 };
 
@@ -81,17 +60,12 @@ function closeBigPicture() {
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onFullScreenEscKeydown);
   buttonUploadComments.classList.remove('hidden');
-  counter = INITIAL_NUMBER_COMMENTS;
-  commentsCount.textContent = ` ${counter} из ${totalComments.textContent} комментариев`;
-
-  for (let index = 0; index < commentsUsers.length - 5; index++) {
-    commentsUsers[index].classList.add('hidden');
-  }
 }
+
 // Передаем данные в комментарии
 
-const avatarUserComment = socialComments.querySelectorAll('.social__picture');
-const textUserComment = socialComments.querySelectorAll('.social__text');
+const avatarUserComment = commentsList.querySelectorAll('.social__picture');
+const textUserComment = commentsList.querySelectorAll('.social__text');
 
 const openUserBigPicture = (miniature) => () => {
   openBigPicture();
