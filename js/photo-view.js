eab6722814035572e4083be4fe00dfbc0b86cdd9
Sergei Fursov <fursov_sergei@mail.ru>
2021-11-17 19:28:19 +0500

    V8

--- js/photo-view.js
+++ js/photo-view.js
@@ -1,93 +1,130 @@
-import {miniatures} from './miniatures.js';
-import {bodyElement} from './upload-file.js';
-import {isEscapeKey} from './util.js';
+import { isEscapeKey } from './util.js';
+import { bodyElement } from './upload-file.js';
+import { miniatures } from './miniatures.js';
 
-
-const bigPictureOpen = document.querySelector('.big-picture');
+const bigPicture = document.querySelector('.big-picture');
 const pictureThumbnails = document.querySelectorAll('.picture');
 const bigPictureImg = document.querySelector('.big-picture__img');
 const imgBig = bigPictureImg.querySelector('img');
 const likesCount = document.querySelector('.likes-count');
-const totalComments= document.querySelector('.comments-count');
+const totalComments = document.querySelector('.comments-count');
 const closeUserBigPicture = document.querySelector('.big-picture__cancel');
 const descriptionBigPicture = document.querySelector('.social__caption');
 const commentsList = document.querySelector('.social__comments');
 const buttonLoadMoreComments = document.querySelector('.comments-loader');
-
+const numberOpenComments = document.querySelector('.social__comment-count');
+const commentsCount = numberOpenComments.firstChild;
+let countOpenComments = 5;
 
 // Закрываем по ESC
 
 const onFullScreenEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) { // )
+  if (isEscapeKey(evt)) {
     evt.preventDefault();
-    bigPictureOpen.classList.add('hidden');
+    bigPicture.classList.add('hidden');
     bodyElement.classList.remove('modal-open');
-
+    buttonLoadMoreComments.classList.remove('hidden');
   }
 };
 
+// Открываем большое фото
+
 function openBigPicture() {
-  bigPictureOpen.classList.remove('hidden');
+  bigPicture.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
+
   document.addEventListener('keydown', onFullScreenEscKeydown);
-
 }
 
-function closeBigPicture() {
-  bigPictureOpen.classList.add('hidden');
+//  Закрываем большое фото
+
+const closeBigPicture = () => {
+  bigPicture.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
+  buttonLoadMoreComments.classList.remove('hidden');
+
+  //counter = INITIAL_NUMBER_COMMENTS;
+  //commentsCount.textContent = ` ${counter} из ${totalComments.textContent} комментариев`;
+
   document.removeEventListener('keydown', onFullScreenEscKeydown);
-
-}
-
-// Передаем данные в комментарии
-
-const avatarUserComment = commentsList.querySelectorAll('.social__picture');
-const textUserComment = commentsList.querySelectorAll('.social__text');
-
-const openUserBigPicture = (miniature) => () => {
-  openBigPicture();
-  imgBig.src = miniature.url;
-  likesCount.textContent = miniature.likes;
-  totalComments.textContent = miniature.comments.length;
-  descriptionBigPicture.textContent = miniature.description;
-
-  miniature.comments.forEach((comment, index) => {
-    avatarUserComment[index].src = comment.avatar;
-    avatarUserComment[index].alt = comment.name;
-    textUserComment[index].textContent = comment.message;
-  });
 };
 
-pictureThumbnails.forEach((thumbnail, index) => {
-  thumbnail.addEventListener('click', openUserBigPicture(miniatures[index]));
-});
+closeUserBigPicture.addEventListener('click', closeBigPicture);
 
-closeUserBigPicture.addEventListener('click', () => {
-  closeBigPicture();
-});
+//const avatarUsers = commentsList.querySelectorAll('.social__picture');
+//const textCommentsUsers = commentsList.querySelectorAll('.social__text');
+//console.log(commentsList);
 
 // Создаем элемент разметки
 
 const socialComment = document.querySelector('.social__comment');
 const commentsFragment = document.createDocumentFragment();
-const createComment = (commentary) => {
+
+const createComment = (comment) => {
   const commentElement = socialComment.cloneNode(true);
   const avatarUser = commentElement.querySelector('.social__picture');
   const textCommentUser = commentElement.querySelector('.social__text');
 
-  avatarUser.src = commentary.avatar;
-  avatarUser.alt = commentary.name;
-  textCommentUser.textContent = commentary.message;
-  commentsFragment.appendChild(commentElement);
-  commentsList.appendChild(commentsFragment);
+  avatarUser.src = comment.avatar;
+  avatarUser.alt = comment.name;
+  textCommentUser.textContent = comment.message;
 
+  commentsFragment.appendChild(commentElement);
+  return commentsFragment;
 };
-let num = 0;
-buttonLoadMoreComments.addEventListener('click', () => {
-  for (let index = num; index < num + 5; index++) {
+
+// Передаем данные
+
+let startIndex = 0;
+const COUNT_COMMENT = 5;
+function addСomments() {
+  for (let index = startIndex; index < startIndex + COUNT_COMMENT; index++) {
     createComment(miniatures[index].comments[index]);
     commentsList.appendChild(commentsFragment);
   }
-  num += 5;
+  startIndex += 5;
+}
+
+const onMiniatureClick = (miniature) => () => {
+  openBigPicture();
+  commentsCount.textContent = ` ${countOpenComments} из `;
+  commentsList.innerHTML = '';
+  addСomments();
+
+  //const avatarsUser = commentsList.querySelectorAll('.social__picture');
+  //const textCommentsUser = commentsList.querySelectorAll('.social__text');
+  //console.log(commentsList);
+  //console.log(avatarsUser);
+  //console.log(avatarsUser);
+
+
+  imgBig.src = miniature.url;
+  likesCount.textContent = miniature.likes;
+  totalComments.textContent = miniature.comments.length;
+  descriptionBigPicture.textContent = miniature.description;
+  /*
+    miniature.comments.forEach((comment, index) => {
+      avatarsUser[index].src = comment.avatar;
+      avatarsUser[index].alt = comment.name;
+      textCommentsUser[index].textContent = comment.message;
+    });
+  */
+};
+
+pictureThumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener('click', onMiniatureClick(miniatures[index]));
 });
+
+// Добавляем комментарии
+
+buttonLoadMoreComments.addEventListener('click', () => {
+  const countTotal = totalComments.textContent;
+  addСomments();
+  countOpenComments += COUNT_COMMENT;
+  commentsCount.textContent = ` ${countOpenComments} из `;
+
+  if (countOpenComments >= countTotal) {
+    buttonLoadMoreComments.classList.add('hidden');
+  }
+});
+
